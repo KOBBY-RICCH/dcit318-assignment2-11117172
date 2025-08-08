771136419972637e12d00c9f4d53895458ee5daf
Kobbyricch <eadjei128@st.ug.edu.gh>
2025-08-08 23:58:21 +0100

    Interfaces

---
+++ Interfaces/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+// Define the interface
+interface IMovable
+{
+    void Move(); 
+}
+
+// Car implements IMovable
+class Car : IMovable
+{
+    public void Move()
+    {
+        Console.WriteLine("Car is moving");
+    }
+}
+
+// Bicycle implements IMovable
+class Bicycle : IMovable
+{
+    public void Move()
+    {
+        Console.WriteLine("Bicycle is moving");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("=== Interface Implementation: IMovable ===");
+
+        IMovable myCar = new Car();
+        IMovable myBicycle = new Bicycle();
+
+        myCar.Move();      
+        myBicycle.Move();  
+    }
+}
